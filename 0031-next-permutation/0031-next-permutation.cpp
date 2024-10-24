cbc8c148af32bc7e90a1e8c9c849d6172e0b85c3
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2024-10-24 20:19:30 +0530

    Time: 0 ms (100.00%), Space: 14.9 MB (48.85%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void nextPermutation(std::vector<int>& a) {
-    int index = -1;
-    int n = a.size();
-    
-    // Search for the break point
-    for (int i = n - 2; i >= 0; i--) {
-        if (a[i] < a[i + 1]) {
-            index = i;
-            break;
+    void nextPermutation(vector<int>& a) {
+        int index = -1;
+        int n = a.size();
+        // searched for break point
+        for(int i=n-2; i>=0; i--) {
+            if(a[i] < a[i+1]) {
+                index = i;
+                break;
+            }
         }
-    }
 
-    // If no break point was found, reverse the whole array
-    if (index == -1) {
-        std::reverse(a.begin(), a.end());
-        return; // Add return to avoid continuing with invalid index
-    }
-
-    // Find the next element greater than a[index] and swap
-    for (int i = n - 1; i > index; i--) {
-        if (a[i] > a[index]) {
-            std::swap(a[i], a[index]);
-            break;
+        if(index == -1) {
+            reverse(a.begin(), a.end());
+            return;
         }
-    }
 
-    // Reverse the portion of the array after the swapped index
-    std::reverse(a.begin() + index + 1, a.end());
-}
+        for(int i = n-1; i> index; i--) {
+            if(a[i] > a[index]) {
+                swap(a[i], a[index]);
+                break;
+            }
+        }
+
+        reverse(a.begin() + index +1, a.end());
+        //return a;
+    }
 };
