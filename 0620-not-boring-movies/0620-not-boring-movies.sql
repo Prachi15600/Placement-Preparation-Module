ea74eae25d0c9f9a2bc780d52ca4ce46c0f52fde
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2024-11-13 23:32:59 +0530

    Time: 314 ms (35.96%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select id, movie, description, rating
 from Cinema
 where id % 2 <> 0 and description <> 'boring' 
