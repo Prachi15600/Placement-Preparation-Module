71ff0e31e02f5ea409727281f2cfb45f272d056f
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2024-11-13 23:33:58 +0530

    Time: 579 ms (17.68%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
-/* Write your T-SQL query statement below */
+/* Write your PL/SQL query statement below */
 select id, movie, description, rating
 from Cinema
-where id % 2 <> 0 and description <> 'boring' 
+where MOD(id,2) <> 0 and description <> 'boring' 
 order by rating desc;
