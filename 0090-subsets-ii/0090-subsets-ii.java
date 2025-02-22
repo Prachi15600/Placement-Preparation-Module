49181bd7575450d3118c15a1120f023dd7b0a25e
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-02-23 01:53:46 +0530

    Time: 1 ms (99.79%), Space: 43.7 MB (44.52%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,18 @@
 class Solution {
-    void subsetsHelper(int[] nums, List<Integer> temp, Set<List<Integer>> res, int index, int n) {
-        if(index == n) {
-            res.add(new ArrayList<>(temp));
-            return;
+    void helper(int[] nums, List<List<Integer>> res, List<Integer> ds, int idx) {
+        res.add(new ArrayList<>(ds));
+        for(int i=idx; i<nums.length; i++) {
+            if(i!=idx && nums[i] == nums[i-1]) continue;
+            ds.add(nums[i]);
+            helper(nums, res, ds, i+1);
+            ds.remove(ds.size()-1);
         }
-        temp.add(nums[index]);
-        subsetsHelper(nums, temp, res, index+1, n);
-        temp.remove(temp.size()-1);
-        subsetsHelper(nums, temp, res, index+1, n);
-
-    } 
-
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        //List<List<Integer>> res = new ArrayList<>();
-        Set<List<Integer>> res = new HashSet<>();
         int n = nums.length;
-        subsetsHelper(nums, new ArrayList<>(), res, 0, n);
-        //set.addAll(res);
-        return new ArrayList<>(res);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums, res, new ArrayList<>(), 0);
+        return res;
     }
 }
