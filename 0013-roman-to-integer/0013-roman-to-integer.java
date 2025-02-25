f48a662a6c47844a1e24d1bdd1a4cf86b19bbec0
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-02-25 10:03:20 +0530

    Time: 3 ms (82.26%), Space: 44.9 MB (51.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -7,43 +7,30 @@ class Solution {
                     sum -= 1;
                 }
                 else sum = sum + 1;
-                //System.out.println(s.charAt(i) + " " + sum);
             }
             else if(s.charAt(i)=='V') {
                 sum = sum + 5;
-                //System.out.println(s.charAt(i) + " " + sum);
-            
             }
             else if(s.charAt(i)=='X') {
                 if(i+1<s.length() && (s.charAt(i+1)=='L' || s.charAt(i+1)=='C')){
                     sum -= 10;
                 }
                 else sum += 10;
-                //System.out.println(s.charAt(i) + " " + sum);
-            
             }
             else if(s.charAt(i)=='L') {
                 sum += 50;
-                System.out.println(s.charAt(i) + " " + sum);
-            
             }
             else if(s.charAt(i)=='C') {
                 if(i+1<s.length() && (s.charAt(i+1)=='D' || s.charAt(i+1)=='M')){
                     sum -= 100;
                 }
                 else sum += 100;
-                //System.out.println(s.charAt(i) + " " + sum);
-            
             }
             else if(s.charAt(i)=='D') {
-                sum += 500;
-                //System.out.println(s.charAt(i) + " " + sum);
-            
+                sum += 500;            
             }
             else if(s.charAt(i)=='M') {
                 sum += 1000;
-                //System.out.println(s.charAt(i) + " " + sum);
-            
             }
 
         }
