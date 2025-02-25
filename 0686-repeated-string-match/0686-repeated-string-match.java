6560cf343235041df77c7a367506e3d659cd360c
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-02-25 13:58:52 +0530

    Time: 268 ms (31.49%), Space: 45.4 MB (11.73%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,25 +1,21 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         String str = a;
-        int m = b.length();
         int count = 1;
+        int m = b.length();
 
         while(str.length() < m) {
             str += a;
             count++;
         }
 
-        if(str.indexOf(b) != -1) {
-            return count;
-        }
+        if(str.indexOf(b) != -1) return count;
 
         str += a;
         count++;
 
-        if(str.indexOf(b) != -1) {
-            return count;
-        }
+        if(str.indexOf(b) != -1) return count;
+
         return -1;
-        
     }
 }
