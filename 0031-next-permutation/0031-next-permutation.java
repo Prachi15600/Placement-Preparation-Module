c904fdff87b49fc8f59b2dc80b455a5b83f720fd
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-03-08 19:14:25 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,45 @@
+import java.util.Arrays;
+
 class Solution {
-    public void nextPermutation(int[] arr) {
-            int n = arr.length;
-            int pivot = -1;
-            for(int i = n - 2 ; i >= 0 ; i--){
-                if(arr[i] < arr[i+1]){
-                    pivot = i;
-                    break;
-                }
-            }
-            
-            if(pivot == -1){
-                reverse(arr, 0, n - 1);
-                return;
-            }
-            for(int i = n - 1; i > pivot; i--){
-                if(arr[i] > arr[pivot]){
-                    int temp = arr[pivot];
-                    arr[pivot] = arr[i];
-                    arr[i] = temp;
-                    break;
-                }
-            }
-            
-            reverse(arr, pivot + 1, n - 1);
-        }
-    
-        // Helper method to reverse array
-        private void reverse(int[] arr, int start, int end) {
-            while (start < end) {
-                swap(arr, start++, end--);
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+
+        // Step 1: Find the first decreasing element from the right
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                index = i - 1;
+                break;
             }
         }
 
-        // Helper method to swap two elements
-        private void swap(int[] arr, int i, int j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        // Step 2: If such an element is found, swap it with the next larger element
+        if (index != -1) {
+            int swapIndex = -1;
+            for (int i = n - 1; i > index; i--) {
+                if (nums[i] > nums[index]) {
+                    swapIndex = i;
+                    break;
+                }
+            }
+
+            // Swap elements
+            int temp = nums[index];
+            nums[index] = nums[swapIndex];
+            nums[swapIndex] = temp;
         }
-    
+
+        // Step 3: Reverse the portion after index to get the next lexicographical order
+        reverse(nums, index + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
