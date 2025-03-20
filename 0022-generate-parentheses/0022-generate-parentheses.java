45755a2082b68840e8129646e356d580e8d6eca4
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-03-20 18:09:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (89.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,35 +1,27 @@
 class Solution {
     List<String> res = new ArrayList<>();
-    boolean isValid(String str) {
-        int count = 0;
-         for(char ch : str.toCharArray()) {
-            if(ch == '(') count++;
-            else count--;
-
-            if (count < 0) return false; 
-         }
-         return count==0;
-    }
-    public void solve(StringBuilder str, int n) {
+    public void solve(StringBuilder str, int n, int open, int close) {
         if(str.length() == 2*n) {
-            if(isValid(str.toString())) {
-                res.add(str.toString());
-            }
+            res.add(str.toString());
             return;
         }
-
-        str.append('(');
-        solve(str, n);
-        str.deleteCharAt(str.length()-1);
-
-        str.append(')');
-        solve(str, n);
-        str.deleteCharAt(str.length()-1);
+        if(open < n) {
+            str.append('(');
+            solve(str, n, open+1, close);
+            str.deleteCharAt(str.length()-1);
+        }
+        
+        if(close < open) {
+            str.append(')');
+            solve(str, n, open, close+1);
+            str.deleteCharAt(str.length()-1);
+        }
+        
     }
     public List<String> generateParenthesis(int n) {
         StringBuilder str = new StringBuilder();
-
-        solve(str, n);
+        int open = 0, close = 0;
+        solve(str, n, open, close);
         return res;
 
         
