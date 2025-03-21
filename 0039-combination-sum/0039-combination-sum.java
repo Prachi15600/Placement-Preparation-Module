05361df718a3da1f50e3aa2c8ce67259e1fb9320
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-03-22 01:12:26 +0530

    Time: 1 ms (99.91%), Space: 44.8 MB (31.85%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,24 @@
 class Solution {
-    void helper(int[] candidates, int target, List<List<Integer>> res, List<Integer> temp, int idx) {
-        if(target == 0) {
-            res.add(new ArrayList<>(temp));
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        generateCombinations(candidates, target, 0, res, new ArrayList<>());
+
+        return res;
+    }
+
+    void generateCombinations(int[] candidates, int target, int i, List<List<Integer>> res, List<Integer> subList) {
+        if(i==candidates.length) {
+            if(target == 0) {
+                res.add(new ArrayList<>(subList));
+            }
             return;
         }
 
-        if(target < 0 ) return;
-
-        for(int i=idx; i<candidates.length; i++) {
-            temp.add(candidates[i]);
-            helper(candidates, target - candidates[i], res, temp, i);
-            temp.remove(temp.size() - 1);
+        if(candidates[i] <= target) {
+            subList.add(candidates[i]);
+            generateCombinations(candidates, target - candidates[i], i, res, subList);
+            subList.remove(subList.size() - 1);
         }
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        helper(candidates, target, res, new ArrayList<>(), 0);
-        return res;
+        generateCombinations(candidates, target, i+1, res, subList);
     }
 }
