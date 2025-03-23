9f14ee8e901bb0b0bb999047eb27849a5164e821
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-03-23 20:42:10 +0530

    Time: 8 ms (54.95%), Space: 56.9 MB (58.37%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,33 +1,35 @@
 class Solution {
-    void helper(String s, List<List<String>> res, int n, int idx, List<String>temp) {
-        if(idx == n) {
-            res.add(new ArrayList(temp));
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        String str = "";
+        generatePartition(s, res, 0, str, new ArrayList<>());
+        return res;
+    }
+
+    void generatePartition(String s, List<List<String>> res, int index, String str, List<String> subList) {
+        if(index == s.length()) {
+            res.add(new ArrayList<>(subList));
             return;
         }
 
-        for(int i=idx; i<n; i++) {
-            if(isPalindrome(s, idx, i)) {
-                temp.add(s.substring(idx, i+1));
-                helper(s, res, n, i+1, temp);
-                temp.remove(temp.size()-1);
+        for(int i=index; i<s.length(); i++) {
+            String subString = s.substring(index, i+1);
+            if(isPalindrome(subString)) {
+                subList.add(subString);
+                generatePartition(s, res, i+1, str, subList);
+                subList.remove(subList.size()-1);
             }
         }
+        
     }
 
-    boolean isPalindrome(String s, int start, int end) {
-        while(start < end) {
-            if(s.charAt(start) != s.charAt(end)) return false;
-            start++;
+    boolean isPalindrome(String st) {
+        int start=0, end = st.length()-1;
+        while(start<=end) {
+            if(st.charAt(start) != st.charAt(end)) return false;
             end--;
+            start++;
         }
         return true;
-    }
-
-    public List<List<String>> partition(String s) {
-        int n = s.length();
-        List<List<String>> res = new ArrayList<>();
-        helper(s, res, n, 0, new ArrayList<>());
-
-        return res;
     }
 }
