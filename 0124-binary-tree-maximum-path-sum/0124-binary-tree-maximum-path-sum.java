1205f7f638a8b065306cd0c9d7c9db9806173bb4
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-04-04 18:01:06 +0530

    Time: 0 ms (100%), Space: 44.6 MB (26.59%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,17 +15,22 @@
  */
 class Solution {
     public int maxPathSum(TreeNode root) {
-        int[] maxi = new int[]{Integer.MIN_VALUE};
-        helper(root, maxi);
-        return maxi[0];
+        int[] res = new int[]{Integer.MIN_VALUE};
+        helper(root, res);
+        return res[0];
     }
 
-    public int helper(TreeNode root, int[] maxi) {
+    int helper(TreeNode root, int[] res) {
         if(root == null) return 0;
-        int lv = Math.max(0,helper(root.left, maxi));
-        int rv = Math.max(0,helper(root.right, maxi));
+        
+        // Ignore negative path sums
+        int lh = Math.max(0, helper(root.left, res));
+        int rh = Math.max(0, helper(root.right, res));
 
-        maxi[0] = Math.max(maxi[0], lv+rv+root.val);
-        return root.val + Math.max(lv,rv);
+        // Update the maximum path sum including the current node
+        res[0] = Math.max(lh + rh + root.val, res[0]);
+
+        // Return max path sum from the current node down
+        return root.val + Math.max(lh, rh);
     }
 }
