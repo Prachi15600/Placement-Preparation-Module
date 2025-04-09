cba9bacd7a9d53e2587d13335a5bcd7b22240707
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-04-10 01:55:34 +0530

    Time: 1 ms (84.92%), Space: 42.1 MB (59.81%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<Integer>PascalList = new ArrayList<>();
-        List<List<Integer>>PascalTriangle = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        PascalList.add(1);
-        PascalTriangle.add(PascalList);
-
-        if (numRows == 1) return PascalTriangle; 
-
-        PascalList = new ArrayList<>();
-        PascalList.add(1);
-        PascalList.add(1);
-        PascalTriangle.add(PascalList);
-
-        for(int i=2; i<numRows; i++) {
-            List<Integer> newRow = new ArrayList<>();
-            newRow.add(1);
-            for(int j=1; j<i; j++) {
-                newRow.add(PascalList.get(j-1)+PascalList.get(j));
+        for(int i=0; i<numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j=0; j<i+1; j++) {
+                if(j==0 || j==i) {
+                    row.add(1);
+                }
+                else {
+                    int num = result.get(i-1).get(j) + result.get(i-1).get(j-1);
+                    row.add(num);
+                }
             }
-            newRow.add(1);
-            PascalTriangle.add(newRow);
-            PascalList = newRow;
+            result.add(row);
         }
-
-        return PascalTriangle;
+        return result;
     }
 }
