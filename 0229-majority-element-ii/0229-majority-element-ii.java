4af38538f5c8c06afa0e5958692080dc4ab814b7
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-04-11 05:09:11 +0530

    Time: 2 ms (99.22%), Space: 48.4 MB (19.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int majorityThreshold = n / 3;
+        int count1 = 0, count2 = 0;
+        int ele1 = 0, ele2 = 0;
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
         for (int num : nums) {
-            hm.put(num, hm.getOrDefault(num, 0) + 1);
-        }
-
-        List<Integer> result = new ArrayList<>();
-        for (int key : hm.keySet()) {
-            if (hm.get(key) > majorityThreshold) {
-                result.add(key);
+            if (count1 == 0 && num != ele2) {
+                count1 = 1;
+                ele1 = num;
+            } else if (count2 == 0 && num != ele1) {
+                count2 = 1; // FIX: should be 1, not 0
+                ele2 = num;
+            } else if (num == ele1) {
+                count1++;
+            } else if (num == ele2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
+
+        // Verify the counts
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == ele1) count1++;
+            if (num == ele2) count2++;
+        }
+
+        int mini = nums.length / 3;
+        List<Integer> result = new ArrayList<>();
+        if (count1 > mini) result.add(ele1);
+        if (ele2 != ele1 && count2 > mini) result.add(ele2); // Avoid duplicates
 
         return result;
     }
