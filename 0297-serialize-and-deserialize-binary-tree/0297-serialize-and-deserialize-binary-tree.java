383a6784339653a02031f3df1e9db8dc32e6d078
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-04-11 10:14:11 +0530

    Time: 9 ms (88.22%), Space: 46.1 MB (47.68%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,37 @@
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val).append(",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] nodes = data.split(",");
+        Queue<String> queue = new LinkedList<>(Arrays.asList(nodes));
+        return deserializeHelper(queue);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> queue) {
+        String val = queue.poll();
+        if (val.equals("null")) return null;
+
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+        root.left = deserializeHelper(queue);
+        root.right = deserializeHelper(queue);
+
+        return root;
+    }
+}
