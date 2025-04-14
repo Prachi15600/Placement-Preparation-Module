7d394ab6a4a1ac522aac1f74e52d76244484a41c
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-04-15 05:05:10 +0530

    Time: 0 ms (100%), Space: 40.7 MB (26.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,23 +1,11 @@
 class Solution {
-    public int countPath(int i, int j, int m, int n, ArrayList<ArrayList<Integer>> dp) {
-        if(i==(m-1) && j==(n-1)) return 1;
-        if(i>=m || j>=n) return 0;
-        if(dp.get(i).get(j) != -1) return dp.get(i).get(j);
-        int right = countPath(i, j+1, m, n, dp);
-        int down = countPath(i+1, j, m, n, dp);
-        dp.get(i).set(j, right+down);
-        return dp.get(i).get(j);
-    }
-
     public int uniquePaths(int m, int n) {
-        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
-        for(int i=0; i<m; i++) {
-            ArrayList<Integer> row = new ArrayList<>();
-            for(int j=0; j<n; j++) {
-                row.add(-1);
-            }
-            dp.add(row);
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        for(int i=1; i<=r; i++) {
+            res = res*(N-r+i)/i;
         }
-        return countPath(0, 0, m, n, dp);
+        return (int)res;
     }
 }
