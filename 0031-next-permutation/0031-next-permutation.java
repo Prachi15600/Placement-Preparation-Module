4da7056058c95a79601a4060ef49987c9c2d88b3
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-06-22 16:46:03 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,34 @@
-import java.util.Arrays;
-
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int index = -1;
 
-        // Step 1: Find the first decreasing element from the right
-        for (int i = n - 1; i > 0; i--) {
-            if (nums[i - 1] < nums[i]) {
-                index = i - 1;
+        for(int i=n-1; i>0; i--) {
+            if(nums[i-1] < nums[i]) {
+                index = i-1;
                 break;
             }
         }
-
-        // Step 2: If such an element is found, swap it with the next larger element
-        if (index != -1) {
-            int swapIndex = -1;
-            for (int i = n - 1; i > index; i--) {
-                if (nums[i] > nums[index]) {
-                    swapIndex = i;
+        if(index != -1) {
+            int indexToSwap = -1;
+            for(int i=n-1; i>index; i--) {
+                if(nums[i] > nums[index]) {
+                    indexToSwap = i;
                     break;
                 }
             }
-
-            // Swap elements
             int temp = nums[index];
-            nums[index] = nums[swapIndex];
-            nums[swapIndex] = temp;
+            nums[index] = nums[indexToSwap];
+            nums[indexToSwap] = temp;
         }
 
-        // Step 3: Reverse the portion after index to get the next lexicographical order
-        reverse(nums, index + 1, n - 1);
+        
+
+        reverse(nums, index+1, n-1);
     }
 
     private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
+        while(start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
