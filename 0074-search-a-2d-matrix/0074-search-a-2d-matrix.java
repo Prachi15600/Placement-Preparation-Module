51f99c92caa7f89aa4f0769712438e3497e1ad21
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-06-27 01:12:05 +0530

    Time: 0 ms (100%), Space: 42.6 MB (18.08%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,26 +3,24 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int low = 0;
-        int high = n * m - 1;
+        int start = 0;
+        int end = n*m-1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while(start <= end) {
+            int mid = start + (end - start) /  2;
+            int midVal = matrix[mid / m][mid % m];  
 
-            // Convert mid index to 2D indices
-            int row = mid / m;
-            int col = mid % m;
-            int midElement = matrix[row][col];
-
-            if (midElement == target) {
+            if(midVal == target) {
                 return true;
-            } else if (target < midElement) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            }
+
+            else if(target < midVal) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
             }
         }
-
         return false;
     }
 }
