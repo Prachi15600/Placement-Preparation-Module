5908f15c8d6d8bdff74a41efce9dbdd8abd2ec26
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-08-08 01:10:13 +0530

    Time: 1 ms (86.09%), Space: 42.1 MB (59.78%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-
+        List<List<Integer>> res = new ArrayList<>();
         for(int i=0; i<numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            for(int j=0; j<i+1; j++) {
+            List<Integer> ans = new ArrayList<>();
+            for(int j=0; j<=i; j++) {
+                int sum = 0;
                 if(j==0 || j==i) {
-                    row.add(1);
+                    ans.add(1);
                 }
                 else {
-                    int num = result.get(i-1).get(j-1) + result.get(i-1).get(j);
-                    row.add(num); 
+                    sum = res.get(i-1).get(j-1) + res.get(i-1).get(j);
+                    ans.add(sum);
                 }
             }
-            result.add(row);
+            res.add(ans);
+
         }
-        return result;
+
+        return res;
     }
 }
