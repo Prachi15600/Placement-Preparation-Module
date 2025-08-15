8c4b66ae10414f0ee5c65bdbcf8a1a2d67217c57
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-08-15 20:49:35 +0530

    Time: 2 ms (99.85%), Space: 50.6 MB (27.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,36 +3,40 @@ class Solution {
         int count1 = 0, count2 = 0;
         int ele1 = 0, ele2 = 0;
 
-        for (int num : nums) {
-            if (count1 == 0 && num != ele2) {
-                count1 = 1;
-                ele1 = num;
-            } else if (count2 == 0 && num != ele1) {
-                count2 = 1; // FIX: should be 1, not 0
-                ele2 = num;
-            } else if (num == ele1) {
+        for(int num : nums) {
+            if(count1 == 0 && num != ele2) {
                 count1++;
-            } else if (num == ele2) {
+                ele1 = num;
+            }
+            else if(count2 == 0 && num != ele1) {
                 count2++;
-            } else {
+                ele2 = num;
+            }
+            else if(num == ele1) {
+                count1++;
+            }
+            else if(num == ele2) {
+                count2++;
+            }
+            else {
                 count1--;
                 count2--;
             }
         }
 
-        // Verify the counts
         count1 = 0;
         count2 = 0;
-        for (int num : nums) {
-            if (num == ele1) count1++;
-            if (num == ele2) count2++;
+        for(int num : nums) {
+            if(num == ele1) count1++;
+            if(num == ele2) count2++;
         }
 
         int mini = nums.length / 3;
-        List<Integer> result = new ArrayList<>();
-        if (count1 > mini) result.add(ele1);
-        if (ele2 != ele1 && count2 > mini) result.add(ele2); // Avoid duplicates
+        List<Integer> res = new ArrayList<>();
+        if(count1 > mini) res.add(ele1);
+        if(ele2 != ele1 && count2 > mini) res.add(ele2);
 
-        return result;
+        return res;
+ 
     }
 }
