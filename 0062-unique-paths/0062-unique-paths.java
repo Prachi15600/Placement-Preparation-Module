ed841371c757a38aa45dceaa3b27134fe2c779e6
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-08-15 21:27:24 +0530

    Time: 0 ms (100%), Space: 40.3 MB (77.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,10 +1,10 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int N = m-1+n-1;
-        int b = n-1;
+        int d = n-1;
         double res = 1;
-        for(int i=1; i<=b; i++) {
-            res = (res*(N-b+i))/i;
+        for(int i=1; i<=d; i++) {
+            res = (res*(N-d+i))/i;
         }
 
         return (int)res;
