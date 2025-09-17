549fc0eaefc70ffef443d4a9b75d22cc577f6ae4
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-09-18 01:18:03 +0530

    Time: 2 ms (66.19%), Space: 42.2 MB (87.3%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,32 +1,30 @@
-class Pair {
-    int i, j;
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
 class Solution {
+    class Pair {
+        int i, j;
+        Pair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
     public int orangesRotting(int[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
         int freshOrange = 0;
         int n = grid.length;
         int m = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(grid[i][j] == 2) {
-                    q.add(new Pair(i,j));
-                }
-                if(grid[i][j] == 1) {
-                    freshOrange++;
-                }
+                if(grid[i][j] == 2) q.add(new Pair(i,j));
+                if(grid[i][j] == 1) freshOrange++;
             }
         }
 
-        if(freshOrange == 0) return 0;
+        if (freshOrange == 0) return 0;
 
         int time = 0;
-        int[][] direction = {{-1,0}, {1,0}, {0, -1}, {0, 1}};
+        int[][] direction = {{-1,0}, {1,0}, {0,-1}, {0, 1}};
+
         while(!q.isEmpty()) {
             int size = q.size();
             while(size > 0) {
@@ -37,7 +35,7 @@ class Solution {
                     int ni = i + dir[0];
                     int nj = j + dir[1];
 
-                    if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1) {
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj] == 1) {
                         grid[ni][nj] = 2;
                         q.add(new Pair(ni, nj));
                         freshOrange--;
@@ -46,9 +44,10 @@ class Solution {
                 size--;
             }
             time++;
-            
         }
+
         if(freshOrange == 0) return time-1;
         return -1;
+
     }
 }
