7660109a666b776bf6278937d63d751c111cef7e
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-09-17 10:45:38 +0530

    Time: 3 ms (46.23%), Space: 42.2 MB (17.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,20 +4,18 @@ class Solution {
         st.push(s.charAt(0));
         for(int i=1; i<s.length(); i++) {
             char c = s.charAt(i);
-            if(c == ')' || c == '}' || c == ']') {
+            if(c == ')' || c == '}' || c == ']' ) {
                 if(st.isEmpty()) return false;
-                char top = st.peek();
-
-                if(c == ')' && top == '(' || c=='}' && top=='{'  || c==']' && top=='[') {
+                if(st.peek() == '(' && c == ')' || st.peek() == '{' && c == '}' || st.peek() == '[' && c == ']') {
                     st.pop();
                 }
                 else return false;
             }
             else {
-                st.push(s.charAt(i));
+                st.push(c);
             }
+
         }
-        
         return st.isEmpty();
     }
 }
