bd186205c285f47e0bea546c823448b3045a6979
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-10-26 15:53:40 +0530

    Time: 5 ms (55.01%), Space: 43.1 MB (76.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
+        int[] count = new int[26];
 
-        int[] count = new int[26]; // only lowercase letters
-
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0; i<s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
 
-        for (int c : count) {
-            if (c != 0) return false;
+        for(int i=0; i<count.length; i++) {
+            if(count[i] != 0) return false;
         }
 
         return true;
     }
 }
-// 0,0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-// a,b,c,d,e,f,g,h,i,j,k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
-// 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
