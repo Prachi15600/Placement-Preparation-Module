d34b27469acef197cf7da04eaa864daf924a6999
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-10-26 14:37:51 +0530

    Time: 0 ms (100%), Space: 18 MB (20.73%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -8,32 +8,32 @@ class Solution:
 
             elif s[i] == 'V':
                 Sum = Sum + 5
-                if i > 0 and s[i-1] == 'I':
+                if i-1 >= 0 and s[i-1] == 'I':
                     Sum = Sum - 2
 
             elif s[i] == 'X':
                 Sum = Sum + 10
-                if i > 0 and s[i-1] == 'I':
+                if i-1 >= 0 and s[i-1] == 'I':
                     Sum = Sum - 2
 
             elif s[i] == 'L':
                 Sum = Sum + 50
-                if i > 0 and s[i-1] == 'X':
+                if i-1 >= 0 and s[i-1] == 'X':
                     Sum = Sum - 20
 
             elif s[i] == 'C':
                 Sum = Sum + 100
-                if i > 0 and s[i-1] == 'X':
+                if i-1 >= 0 and s[i-1] == 'X':
                     Sum = Sum - 20
 
             elif s[i] == 'D':
                 Sum = Sum + 500
-                if i > 0 and s[i-1] == 'C':
+                if i-1 >= 0 and s[i-1] == 'C':
                     Sum = Sum - 200
 
             elif s[i] == 'M':
                 Sum = Sum + 1000
-                if i > 0 and s[i-1] == 'C':
+                if i-1 >= 0 and s[i-1] == 'C':
                     Sum = Sum - 200
 
         return Sum
