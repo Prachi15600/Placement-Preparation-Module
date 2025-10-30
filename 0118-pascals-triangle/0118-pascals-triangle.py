97ada1dccfb9c31a0aeb3ee7165ef6be22f975be
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-10-30 20:28:39 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,11 +5,12 @@ class Solution:
         for i in range(numRows) :
             ans = []
             for j in range(i+1):
-                if j == 0 or j == i:
+                if j==0 or j==i:
                     ans.append(1)
 
-                else :
-                    ans.append(res[i-1][j-1] + res[i-1][j])
+                else:
+                    sum = res[i-1][j-1] + res[i-1][j]
+                    ans.append(sum)
+
             res.append(ans)
         return res
-        
