a8734021e17ee2f8cc9f3904bee6ccac60193c1e
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-10-30 20:23:56 +0530

    Time: 1 ms (86.21%), Space: 42 MB (62.52%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -6,11 +6,12 @@ class Solution {
             List<Integer> ans = new ArrayList<>();
             for(int j=0; j<=i; j++) {
                 int sum = 0;
-                if(j==i || j==0) {
+                if(j==0 || j==i) {
                     ans.add(1);
                 }
                 else {
-                    ans.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+                    sum = res.get(i-1).get(j-1) + res.get(i-1).get(j);
+                    ans.add(sum);
                 }
             }
             res.add(ans);
