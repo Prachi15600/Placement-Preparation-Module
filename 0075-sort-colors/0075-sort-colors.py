19d2c4a350d1858b8794b8589cf8863f0cce3767
Prachi Yadav <88781651+Prachi15600@users.noreply.github.com>
2025-10-30 21:05:18 +0530

    Time: 0 ms (100%), Space: 18 MB (11.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,23 +5,21 @@ class Solution:
         count2 = 0
 
         for i in range(len(nums)) :
-            if nums[i] == 0:
-                count0 += 1
-            elif nums[i] == 1:
-                count1 += 1
-            else:
-                count2 += 1
-    
+            if nums[i] == 0: count0 += 1
+            elif nums[i] == 1: count1 += 1
+            else : count2 += 1
+
         index = 0
-        for i in range(count0):
+
+        for i in range(count0) :
             nums[index] = 0
-            index += 1
+            index = index + 1
 
-        for i in range(count1):
+        for i in range(count1) :
             nums[index] = 1
-            index += 1
+            index = index + 1
 
-        for i in range(count2):
+        for i in range(count2) :
             nums[index] = 2
-            index += 1            
+            index = index + 1
         
